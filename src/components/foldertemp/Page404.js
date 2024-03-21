f8fa9fb4721c6cbb820125944693a62f7e9edf56
oipik <wazuup.lw@mail.ru>
2024-03-21 21:18:48 +0300

    temp folder

---
+++ src/components/foldertemp/Page404.js
@@ -0,0 +1,13 @@
+
+const Page404 = () => {
+  return (
+    <div className="w-full mx-auto text-center">
+      <p className="font-bold text-4xl">404</p>
+      <p className="text-2xl leading-10">File not found</p> 
+      <p className="text-2xl leading-10">The site configured at this address does not contain the requested file.</p>
+      <p className="text-2xl leading-10">If this is your site, make sure that the filename case matches the URL as well as any file permissions.</p>
+    </div>
+  )
+}
+
+export default Page404;
